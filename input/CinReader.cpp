a413a2e95192ed1398a86383e21d575f2f7b1acd
vpaladii <samaelxxi@gmail.com>
2019-10-19 22:12:41 +0300

    added cin read

--- input/CinReader.cpp
+++ input/CinReader.cpp
@@ -2,10 +2,23 @@
 // Created by Vladyslav PALADII on 2019-10-19.
 //
 
+#include <iostream>
 #include "CinReader.h"
 
 std::shared_ptr<std::vector<std::string>> CinReader::readLines() const {
     auto lines = std::make_shared<std::vector<std::string>>();
+
+
+    std::string line;
+    while (true) {
+        std::cin >> line;
+        if (line == ";;")
+            break;
+        lines->push_back(line);
+    }
+
+    for (auto &l : *lines)
+        std::cout << l << std::endl;
 
     return lines;
 }
